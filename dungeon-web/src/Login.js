3305566217691bef6a84176de89ee7679fc23768
Riksulee <riikka.valjakka@gmail.com>
2018-08-20 17:06:30 +0300

    Login page with form and name search

---
+++ dungeon-web/src/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { getUser } from "./Service";
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ''
+        }
+    }
+
+    nameChanged = (e) => {
+        this.setState({ name: e.target.value });
+    }
+
+    login = (e) => {
+        e.preventDefault();
+        getUser(this.state.name, function() {
+            this.setState({ name: ''});
+        }.bind(this));
+    }
+
+
+    render() {
+        return (
+            <div className="Login">
+                <form onSubmit={this.login}>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Name: </td><td><input value={this.state.name} onChange={this.nameChanged}/></td>
+                            </tr>
+                            <tr>
+                                <td><input type="submit" defaultValue="Login"/></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
---
+++ dungeon-web/src/Service.js
@@ -0,0 +1,122 @@
+import fetchival from 'fetchival';
+
+const baseurl = "http://rem-rest-api.herokuapp.com/api/users";
+
+// function haeTapahtumaLista(callback) {
+//     fetch(baseurl)
+//         .then(function (response) {
+//             if (!response.ok) {
+//                 const errviesti = {
+//                     status: response.status,
+//                     statusText: response.statusText,
+//                     viesti: "Listanhaku"
+//                 };
+//                 throw errviesti;
+//             }
+//             return response.json()
+//         })
+//         .then(function (lista) {
+//             callback(lista);
+//         });
+// }
+
+// export function luoEvent(event, callback) {
+//     return fetch(baseurl, {
+
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(event)
+//     })
+
+//         .then(function (response) {
+//             callback(response.status);
+//         });
+// }
+
+// function poistaEvent(id) {
+
+//     return fetch(baseurl + "?id=" + id, {
+
+//         method: 'DELETE',
+//     });
+// }
+
+// function haeKayttajaLista(callback) {
+//     fetch(baseurlUser)
+//         .then(function (response) {
+//             if (!response.ok) {
+//                 const errviesti = {
+//                     status: response.status,
+//                     statusText: response.statusText,
+//                     viesti: "Listanhaku"
+//                 };
+//                 throw errviesti;
+//             }
+//             return response.json()
+//         })
+//         .then(function (lista) {
+//             callback(lista);
+//         });
+// }
+
+// export function luoKayttaja(event) {
+//     return fetch(baseurlUser, {
+
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(event)
+//     });
+        
+// }
+
+// function poistaKayttaja(id) {
+
+//     return fetch(baseurlUser + "?id=" + id, {
+//         method: 'DELETE',
+//     });
+// }
+// playerName
+
+async function getUser(name) {
+    var players = fetchival(baseurl);
+    console.log("jippii fetch");
+    const kyrb = await players.get();
+    var player = (kyrb['data'].find(a => a.firstName === name));
+    console.log(player);
+    // find(a => a.firstName === name));
+
+    // { name: 'cherries', quantity: 5 }
+    // fetch("api/players", {
+    //     method: 'GET',
+    // })
+
+    //     .then(function (response) {
+    //         if (!response.ok) {
+    //             alert("An error occured");
+    //             const errMessage = {
+    //                 status: response.status,
+    //                 statusText: response.statusText,
+    //                 message: "Player search"
+    //             };
+    //             throw errMessage;
+    //         }
+    //         return response.json()
+    //     })
+        // .then(function (olio) {
+        //     if (olio.pw === password) {
+                
+        //         //VIIME HETKEN MUUTOS
+        //         console.log("Täsmää!");
+        //         cb(olio);
+        //     }
+        //     else {
+        //         console.log("Ei täsmää");
+        //         alert("Virheellinen salasana tai sähköpostioisoite");
+        //     }
+        // });
+
+}
+
+export { getUser }
+
+
