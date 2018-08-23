fde0d27cffeb7a1bb558b4041f048d32cffe1359
Riksulee <riikka.valjakka@gmail.com>
2018-08-23 15:10:16 +0300

    Constructor added on sessionPage.js

--- dungeon-web/src/profilePage.js
+++ dungeon-web/src/profilePage.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import Sessions from './Sessions';
-import SessionPage from './sessionPage';
 import Login from './Login';
 
 class ProfilePage extends Component {
     render() {
         return (
-            <div className="sessionList">              
+            <div className="sessionList">
                 <Login />
                 <Sessions />
             </div>
         );
     };
-
 }
 
 export default ProfilePage;
