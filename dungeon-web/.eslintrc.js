03dcecd270afb5b08b7c8e64d94bbd2f42dc5cfd
Oscar Storbacka <ostorbacka@gmail.com>
2018-08-28 19:24:12 +0300

    Enable ESLint

---
+++ dungeon-web/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": ["airbnb"],
+    "parser": "babel-eslint",
+    "rules": {
+        "react/jsx-filename-extension": [
+          "error",
+          {
+            "extensions": [".js", ".jsx"]
+          }
+        ]
+    }
+}
